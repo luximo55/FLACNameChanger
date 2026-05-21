fc49ebd41a5b9afb0342b92af45b974281ce6c54
luximo55 <luximo3@gmail.com>
2026-05-21 15:22:16 +0200

    Final

---
+++ FNC.py
@@ -0,0 +1,17 @@
+import os;
+
+dirChoice = input("Choose Directory:")
+os.chdir(dirChoice)
+for x, folder in enumerate(os.listdir()):
+    print(dirChoice + "/" + folder)
+    os.chdir(dirChoice + "/" + folder)
+    for count, file in enumerate(sorted(os.listdir())):
+        fileName, ext = os.path.splitext(file)
+        if (ext != '.flac') and (ext != '.mp3') and (ext != '.txt'):
+            break
+        songName = fileName.split('-')
+        fileName = '0' + str(count + 1) + ' -' + songName[1];  
+        
+        new_name = f'{fileName}{ext}'
+        os.rename(file, new_name)
+        print("Changed file " + file + " to " + new_name)
