02f9989e249cca6c8ee36adbc1242479bd33062d
luximo55 <luximo3@gmail.com>
2026-06-09 14:17:09 +0200

    Fixed a bug

--- FNC.py
+++ FNC.py
@@ -10,8 +10,10 @@ for x, folder in enumerate(os.listdir()):
         if (ext != '.flac') and (ext != '.mp3') and (ext != '.txt'):
             break
         songName = fileName.split('-')
-        fileName = '0' + str(count + 1) + ' -' + songName[1];  
-        
+        if count+1 < 10:
+            fileName = '0' + str(count + 1) + ' -' + songName[1];
+        else:
+            fileName = str(count + 1) + ' -' + songName[1];
         new_name = f'{fileName}{ext}'
         os.rename(file, new_name)
         print("Changed file " + file + " to " + new_name)
